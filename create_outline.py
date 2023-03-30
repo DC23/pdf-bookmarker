4eaaa499dbe0cb7acb579f48f9f3a42ec4643d83
DC23 <jugglindan@gmail.com>
2023-03-30 19:34:58 +1100

    adding offset between TOC page and PDF actual page

--- create_outline.py
+++ create_outline.py
@@ -10,16 +10,17 @@ def split_line(line):
     return match.groups()
 
 
-def parse_toc(page:int, input_pdf:str):
+def parse_toc(page:int, offset:int, input_pdf:str):
     doc = fitz.open(input_pdf)
     toc_page = doc.load_page(page)
     for line in toc_page.get_text().split("\n"):
         # print(line)
         try:
             name, page = split_line(line)
+            page = int(page) + offset
             if name.find("PART") > -1:
                 print(f"{name}: {page}")
-            else: 
+            else:
                 print(f"\t{name}: {page}")
         except:
             print(line)
@@ -27,13 +28,14 @@ def parse_toc(page:int, input_pdf:str):
 
 @click.command()
 @click.option("--toc-page", type=click.INT, help="PDF page containing the table of contents")
-@click.option("--input_pdf", help="The input PDF")
-@click.option("--output_pdf", help="The output PDF")
-def run(toc_page:int, input_pdf:str, output_pdf:str):
+@click.option("--input-pdf", help="The input PDF")
+@click.option("--output-pdf", help="The output PDF")
+@click.option("--page-offset", type=click.INT, default=0, help="Offset between the displayed page number and the actual PDF page number")
+def run(toc_page:int, page_offset:int, input_pdf:str, output_pdf:str):
     """Parse a Basic Fantasy PDF file and automatically generate the PDF bookmarks
 Version: 0.0.2
     """
-    parse_toc(toc_page, input_pdf)
+    parse_toc(toc_page, page_offset, input_pdf)
 
 if __name__ == '__main__':
     run()
