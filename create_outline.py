2130ffb17a713304191a8f10ed28c014d06a8bc4
DC23 <jugglindan@gmail.com>
2023-03-30 18:01:28 +1100

    adding command-line args

--- create_outline.py
+++ create_outline.py
@@ -1,3 +1,4 @@
+import click
 import fitz
 import re
 
@@ -9,9 +10,9 @@ def split_line(line):
     return match.groups()
 
 
-def test_fitz():
-    doc = fitz.open("input.pdf")
-    toc_page = doc.load_page(2)
+def parse_toc(page:int, input_pdf:str):
+    doc = fitz.open(input_pdf)
+    toc_page = doc.load_page(page)
     for line in toc_page.get_text().split("\n"):
         # print(line)
         try:
@@ -24,6 +25,12 @@ def test_fitz():
             print(line)
 
 
+@click.command()
+@click.option("--toc-page", type=click.INT, help="PDF page containing the table of contents")
+@click.option("--input_pdf", help="The input PDF")
+@click.option("--output_pdf", help="The output PDF")
+def run(toc_page:int, input_pdf:str, output_pdf:str):
+    parse_toc(toc_page, input_pdf)
 
 if __name__ == '__main__':
-    test_fitz()
+    run()
